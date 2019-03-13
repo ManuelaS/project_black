61c0abefa2f38a3c119d542ad8e18821da6adfcb
Manuela Salvucci <manuelasalvucci@rcsi.com>
2019-03-13 17:59:49 +0000

    Added imputation for redundant zone1 positional information

--- run_exploratory_analyses.py
+++ run_exploratory_analyses.py
@@ -19,6 +19,7 @@ def read_data():
          - Is_Weekend: "binary (1 for weekend and 0 for weekday)."""
 
     data = pandas.read_csv(os.path.join('data', 'WIDS_Project_Generated_Data_10K.csv'), index_col=0)
+    # data = pandas.read_csv(os.path.join('data', 'WIDS_Dataset_Full_Aug18_Jan19_Adjusted.csv'), index_col=0)
     data.index.name = 'row_id'
 
     # Add binary Result_Type_Bin variable
@@ -36,6 +37,63 @@ def read_data():
     data['Is_Weekend'] = (pandas.to_datetime(data.Date).dt.dayofweek // 5 == 1).astype(float)
 
     return data
+
+def impute_data_zone1(data):
+    # Columns related to position in zone1
+    zone1_pos = ['Zone1Position', 'Zone1_Row_Num', 'Zone1_Col_Num', 'Zone1_Left_Block_Bin', 'Zone1_Right_Block_Bin', 'Zone1_Area']
+    # Investigate patterns of missing data:
+    print(data[zone1_pos].isna().groupby(zone1_pos).size())
+
+    print('{:.2f}% fraction of samples have missing positional information for Zone1'.format(
+        100 * data[zone1_pos].isna().any(axis=1).mean()))
+    print(
+        'but complete positional information can be made out from Zone1Position except when missing. Zone1Position is '
+        'missing for {:.2f}% of the samples)'.format(100 * data.Zone1Position.isna().mean()))
+
+    print('If Zone1Position information is available, all other position columns can be inferred.')
+    print('Zone1Position can be inferred from Row+Col, when both are present')
+    print(
+        'If Col is missing, we will be unable to determine exact Zone1Position, as this is not available anywhere else')
+    print('If Row is missing it can be inferred from Zone1_Area')
+
+    # Set Row based on Zone1_Area where we can.
+    data.loc[data.Zone1_Area.isin(['Top Right', 'Top Left']), 'Zone1_Row_Num'] = 1
+    data.loc[data.Zone1_Area.isin(['Bottom Right', 'Bottom Left']), 'Zone1_Row_Num'] = 2
+
+    # Input values in 'Zone1Position' based on row and col position
+    data.loc[(data.Zone1_Row_Num == 1) & (data.Zone1_Col_Num == 1), 'Zone1Position'] = 1
+    data.loc[(data.Zone1_Row_Num == 1) & (data.Zone1_Col_Num == 2), 'Zone1Position'] = 2
+    data.loc[(data.Zone1_Row_Num == 1) & (data.Zone1_Col_Num == 3), 'Zone1Position'] = 3
+    data.loc[(data.Zone1_Row_Num == 1) & (data.Zone1_Col_Num == 4), 'Zone1Position'] = 4
+    data.loc[(data.Zone1_Row_Num == 2) & (data.Zone1_Col_Num == 1), 'Zone1Position'] = 5
+    data.loc[(data.Zone1_Row_Num == 2) & (data.Zone1_Col_Num == 2), 'Zone1Position'] = 6
+    data.loc[(data.Zone1_Row_Num == 2) & (data.Zone1_Col_Num == 3), 'Zone1Position'] = 7
+    data.loc[(data.Zone1_Row_Num == 2) & (data.Zone1_Col_Num == 4), 'Zone1Position'] = 8
+
+    # Now that 'Zone1Position' is set, use this info to impute values in remaining columns
+    data.loc[data.Zone1Position.isin([1, 2, 3, 4]), 'Zone1_Row_Num'] = 1
+    data.loc[data.Zone1Position.isin([5, 6, 7, 8]), 'Zone1_Row_Num'] = 2
+
+    data.loc[data.Zone1Position.isin([1, 5]), 'Zone1_Col_Num'] = 1
+    data.loc[data.Zone1Position.isin([2, 6]), 'Zone1_Col_Num'] = 2
+    data.loc[data.Zone1Position.isin([3, 7]), 'Zone1_Col_Num'] = 3
+    data.loc[data.Zone1Position.isin([4, 8]), 'Zone1_Col_Num'] = 4
+
+    data.loc[data.Zone1Position.isin([1, 2, 5, 6]), 'Zone1_Left_Block_Bin'] = 1
+    data.loc[data.Zone1Position.isin([3, 4, 7, 8]), 'Zone1_Left_Block_Bin'] = 0
+
+    data.loc[data.Zone1Position.isin([3, 4, 7, 8]), 'Zone1_Right_Block_Bin'] = 1
+    data.loc[data.Zone1Position.isin([1, 2, 5, 6]), 'Zone1_Right_Block_Bin'] = 0
+
+    data.loc[data.Zone1Position.isin([1, 2]), 'Zone1_Area'] = 'Top Left'
+    data.loc[data.Zone1Position.isin([3, 4]), 'Zone1_Area'] = 'Top Right'
+    data.loc[data.Zone1Position.isin([5, 6]), 'Zone1_Area'] = 'Bottom Left'
+    data.loc[data.Zone1Position.isin([7, 8]), 'Zone1_Area'] = 'Bottom Right'
+
+    # Investigate remaining patterns of missing data:
+    print('After imputation {:.2f}% fraction of samples have missing positional information for Zone1'.format(
+        100 * data[zone1_pos].isna().any(axis=1).mean()))
+    print(data[zone1_pos].isna().groupby(zone1_pos).size())
 
 
 def plot_var_by_SKU_and_result_type(data, cols):
@@ -118,6 +176,9 @@ if __name__ == '__main__':
     # Drop un-informative columns
     data.drop(columns=['Block_Orientation'], inplace=True) # Same value in column
 
+    # Impute missing data from redundant information
+    impute_data_zone1(data)
+
     # Drop entries with missing data
     # TODO: Revisit after coming up with a strategy to deal with missing data
     data = data.dropna()
@@ -164,7 +225,7 @@ if __name__ == '__main__':
 
     # Influence of date on defects broken down by SKU
     plot_var_by_date_and_SKU(data, ['Passed_QC_Count'])
-    plot_var_by_date_and_SKU(data, ['Passed_QC_Count', 'Defect_1_Count', 'Defect_2_Count', 'Defect_3_Count',
+    plot_var_by_date_and_SKU(data, ['Defect_1_Count', 'Defect_2_Count', 'Defect_3_Count',
                                     'Defect_4_Count'])
 
 
