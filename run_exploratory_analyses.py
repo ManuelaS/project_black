8655547f85add91e88cb01ec812c6cf199aa7097
Manuela Salvucci <manuelasalvucci@rcsi.com>
2019-03-14 20:18:46 +0000

    Added imputation of Zone2Position and Zone3Position data based on redundant position columns.

--- run_exploratory_analyses.py
+++ run_exploratory_analyses.py
@@ -38,23 +38,15 @@ def read_data():
 
     return data
 
+
 def impute_data_zone1(data):
     # Columns related to position in zone1
     zone1_pos = ['Zone1Position', 'Zone1_Row_Num', 'Zone1_Col_Num', 'Zone1_Left_Block_Bin', 'Zone1_Right_Block_Bin', 'Zone1_Area']
-    # Investigate patterns of missing data:
-    print(data[zone1_pos].isna().groupby(zone1_pos).size())
+    # Investigate patterns of available data:
+    print(data[zone1_pos].notna().groupby(zone1_pos).size())
 
-    print('{:.2f}% fraction of samples have missing positional information for Zone1'.format(
-        100 * data[zone1_pos].isna().any(axis=1).mean()))
-    print(
-        'but complete positional information can be made out from Zone1Position except when missing. Zone1Position is '
-        'missing for {:.2f}% of the samples)'.format(100 * data.Zone1Position.isna().mean()))
-
-    print('If Zone1Position information is available, all other position columns can be inferred.')
-    print('Zone1Position can be inferred from Row+Col, when both are present')
-    print(
-        'If Col is missing, we will be unable to determine exact Zone1Position, as this is not available anywhere else')
-    print('If Row is missing it can be inferred from Zone1_Area')
+    print('{:.2f}% fraction of samples have positional information for Zone1'.format(
+        100 * data.Zone1Position.notna().mean()))
 
     # Set Row based on Zone1_Area where we can.
     data.loc[data.Zone1_Area.isin(['Top Right', 'Top Left']), 'Zone1_Row_Num'] = 1
@@ -73,8 +65,58 @@ def impute_data_zone1(data):
     data.drop(columns=set(zone1_pos).difference(['Zone1Position']), inplace=True)
 
     # Investigate remaining patterns of missing data:
-    print('After imputation {:.2f}% fraction of samples have missing positional information for Zone1'.format(
-        100 * data['Zone1Position'].isna().mean()))
+    print('{:.2f}% fraction of samples have positional information for Zone1 after imputation.'.format(
+        100 * data.Zone1Position.notna().mean()))
+
+
+def impute_data_zone2(data):
+    # Columns related to position in zone2
+    zone2_pos = ['Zone2Position', 'Zone2_Row_Num', 'Zone2_Col_num']
+    # Investigate patterns of available data:
+    print(data[zone2_pos].notna().groupby(zone2_pos).size())
+
+    print('{:.2f}% fraction of samples have positional information for Zone2'.format(
+        100 * data.Zone2Position.notna().mean()))
+
+    # Input values in 'Zone2Position' based on row and col position
+    data.loc[(data.Zone2_Row_Num == 1) & (data.Zone2_Col_num == 1), 'Zone2Position'] = 1
+    data.loc[(data.Zone2_Row_Num == 1) & (data.Zone2_Col_num == 2), 'Zone2Position'] = 2
+    data.loc[(data.Zone2_Row_Num == 2) & (data.Zone2_Col_num == 1), 'Zone2Position'] = 3
+    data.loc[(data.Zone2_Row_Num == 2) & (data.Zone2_Col_num == 2), 'Zone2Position'] = 4
+
+    data.drop(columns=set(zone2_pos).difference(['Zone2Position']), inplace=True)
+
+    print('{:.2f}% fraction of samples have positional information for Zone2 after imputation.'.format(
+        100 * data.Zone2Position.notna().mean()))
+
+
+def impute_data_zone3(data):
+    # Columns related to position in zone3
+    zone3_pos = ['Zone3Position', 'Zone3_Row_Num', 'Zone3_Col_Num', 'Zone3_Area']
+    # Investigate patterns of available data:
+    print(data[zone3_pos].notna().groupby(zone3_pos).size())
+
+    print('{:.2f}% fraction of samples have positional information for Zone3'.format(
+        100 * data.Zone3Position.notna().mean()))
+
+    # Set Position based on Zone3_Area and Row/Column where we can.
+    data.loc[(data.Zone3_Area == 'Top Left') & (data.Zone3_Col_Num == 2), 'Zone3Position'] = 2
+    data.loc[(data.Zone3_Area == 'Top Left') & (data.Zone3_Row_Num == 2), 'Zone3Position'] = 4
+    data.loc[(data.Zone3_Area == 'Bottom Right') & (data.Zone3_Row_Num == 1), 'Zone3Position'] = 3
+    data.loc[(data.Zone3_Area == 'Bottom Right') & (data.Zone3_Col_Num == 2), 'Zone3Position'] = 5
+
+    # Input values in 'Zone2Position' based on row and col position
+    data.loc[(data.Zone3_Row_Num == 1) & (data.Zone3_Col_Num == 1), 'Zone3Position'] = 1
+    data.loc[(data.Zone3_Row_Num == 1) & (data.Zone3_Col_Num == 2), 'Zone3Position'] = 2
+    data.loc[(data.Zone3_Row_Num == 1) & (data.Zone3_Col_Num == 3), 'Zone3Position'] = 3
+    data.loc[(data.Zone3_Row_Num == 2) & (data.Zone3_Col_Num == 1), 'Zone3Position'] = 4
+    data.loc[(data.Zone3_Row_Num == 2) & (data.Zone3_Col_Num == 2), 'Zone3Position'] = 5
+    data.loc[(data.Zone3_Row_Num == 2) & (data.Zone3_Col_Num == 3), 'Zone3Position'] = 6
+
+    data.drop(columns=set(zone3_pos).difference(['Zone3Position']), inplace=True)
+
+    print('{:.2f}% fraction of samples have positional information for Zone3 after imputation.'.format(
+        100 * data.Zone3Position.notna().mean()))
 
 
 def plot_var_by_SKU_and_result_type(data, cols):
@@ -159,10 +201,14 @@ if __name__ == '__main__':
 
     # Impute missing data from redundant information
     impute_data_zone1(data)
+    impute_data_zone2(data)
+    impute_data_zone3(data)
+    print('After removing missing data for positions in zones 1-3, dataset with {} rows and {} columns'.
+          format(data.shape[0], data.shape[1]))
 
-    # Drop entries with missing data
-    # TODO: Revisit after coming up with a strategy to deal with missing data
-    data = data.dropna()
+    # # Drop entries with missing data
+    # # TODO: Revisit after coming up with a strategy to deal with missing data
+    # data = data.dropna()
     print('After removing missing data, dataset with {} rows and {} columns'.format(data.shape[0], data.shape[1]))
 
     # Exploratory plots
